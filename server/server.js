2def34fbddebd281a645550b0feb1f8697aeeb7c
Justin Hynes-Bruell <hynes-bruell.j@husky.neu.edu>
2018-02-19 23:16:44 -0500

    Changed server port 2

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 
-app.set('port', process.env.PORT || 8083);
+app.set('port', 8083);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
